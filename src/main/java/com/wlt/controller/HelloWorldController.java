04926105ab1b3d0163c77bce6a3ea31236525670
魏霖涛 <3263138624@qq.com>
2017-09-14 10:03:02 +0800

    springboot入门denmo：包含集合tomcat webmvc

---
+++ src/main/java/com/wlt/controller/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.wlt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *类描述：
+ *@author: 魏霖涛
+ *@date： 日期：2017-3-31 时间：上午10:42:08
+ *@version 1.0
+ */
+@RestController
+/**
+ * 备注：@RestController的意思就是controller里面的方法都以json格式输出，不用再写什么jackjson配置的了
+ * @author wlt
+ *
+ */
+public class HelloWorldController {
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+}
